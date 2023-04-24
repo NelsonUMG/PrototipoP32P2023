710bba6106e9374da710aeeaac46a2cc3bb6300c
NelsonUMG <npinedac3@miumg.edu.gt>
2023-04-24 12:46:06 -0600

    agregando cambios en el daoBancos

--- Proyectop32k23/src/main/java/Seguridad/Modelo/daoBancos.java
+++ Proyectop32k23/src/main/java/Seguridad/Modelo/daoBancos.java
@@ -5,43 +5,45 @@
  */
 package Seguridad.Modelo;
 
-import Seguridad.Controlador.clsAplicacion;
+import Seguridad.Controlador.clsBancos;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
+// private int tipoPersona;
+  //  private String descripcion;
+   //git  private String Estatus;
 /**
  *
  * @author visitante
  */
 public class daoBancos {
 
-    private static final String SQL_SELECT = "SELECT aplid, aplnombre, aplestatus FROM tbl_aplicacion";
-    private static final String SQL_INSERT = "INSERT INTO tbl_aplicacion(aplnombre, aplestatus) VALUES(?, ?)";
-    private static final String SQL_UPDATE = "UPDATE tbl_aplicacion SET aplnombre=?, aplestatus=? WHERE aplid = ?";
-    private static final String SQL_DELETE = "DELETE FROM tbl_aplicacion WHERE aplid=?";
-    private static final String SQL_SELECT_NOMBRE = "SELECT aplid, aplnombre, aplestatus FROM tbl_aplicacion WHERE aplnombre = ?";
-    private static final String SQL_SELECT_ID = "SELECT aplid, aplnombre, aplestatus FROM tbl_aplicacion WHERE aplid = ?";    
+    private static final String SQL_SELECT = "SELECT tipoPersona, descripcion, Estatus FROM tbl_bancos";
+    private static final String SQL_INSERT = "INSERT INTO tbl_bancos(tipoPersona, descripcion,  Estatus) VALUES(?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE tbl_bancos SET tipoPersona=?, Estatus=? WHERE tipoPersona = ?";
+    private static final String SQL_DELETE = "DELETE FROM tbl_bancos WHERE tipoPersona=?";
+    private static final String SQL_SELECT_NOMBRE = "SELECT tipoPersona, descripcion,  Estatus FROM tbl_bancos WHERE descripcion = ?";
+    private static final String SQL_SELECT_ID = "SELECT tipoPersona, descripcion, Estatus FROM tbl_bancos WHERE tipoPersona = ?";    
 
-    public List<clsAplicacion> consultaAplicacion() {
+    public List<clsBancos> consultaBanco() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         
-        List<clsAplicacion> aplicaciones = new ArrayList<>();
+        List<clsBancos> bancos = new ArrayList<>();
         try {
             conn = Conexion.getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
             rs = stmt.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt("aplid");
-                String nombre = rs.getString("aplnombre");
-                String estatus = rs.getString("aplestatus");
-                clsAplicacion aplicacion = new clsAplicacion();
-                aplicacion.setIdAplicacion(id);
-                aplicacion.setNombreAplicacion(nombre);
-                aplicacion.setEstatusAplicacion(estatus);
-                aplicaciones.add(aplicacion);
+                String tipo = rs.getString("tipoPersona"); 
+                String nombre = rs.getString("descripcion");
+                String estatus = rs.getString("Estatus");
+                clsBancos banco = new clsBancos();
+                banco.setTipoPersona(tipo);
+                banco.setDescripcion(nombre);
+                banco.setEstatus(estatus);
+                bancos.add(banco);
             }
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
@@ -50,18 +52,19 @@ public class daoBancos {
             Conexion.close(stmt);
             Conexion.close(conn);
         }
-        return aplicaciones;
+        return bancos;
     }
 
-    public int ingresaAplicacion(clsAplicacion aplicacion) {
+    public int ingresaBanco(clsBancos banco) {
         Connection conn = null;
         PreparedStatement stmt = null;
         int rows = 0;
         try {
             conn = Conexion.getConnection();
             stmt = conn.prepareStatement(SQL_INSERT);
-            stmt.setString(1, aplicacion.getNombreAplicacion());
-            stmt.setString(2, aplicacion.getEstatusAplicacion());
+            stmt.setString(1, banco.getTipoPersona());
+            stmt.setString(2, banco.getDescripcion());
+            stmt.setString(3, banco.getEstatus());
 
             System.out.println("ejecutando query:" + SQL_INSERT);
             rows = stmt.executeUpdate();
